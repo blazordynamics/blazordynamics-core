cf287a73c7bb664b0f4d8ea969b7eee8047b923d
Frans van Ek <frans@fransvanek.nl>
2024-06-30 18:25:05 +0200

    fixed some sonar cloud issues

--- src/BlazorDynamics.Components/Builders/FormComponentBuilder.cs
+++ src/BlazorDynamics.Components/Builders/FormComponentBuilder.cs
@@ -15,109 +15,104 @@ namespace BlazorDynamics.Forms.Components.Builders
         private readonly ParameterList _customParameters = new ParameterList();
         public TBuilder WithComponentDefinitions(ComponentsList components)
         {
-            _component.Components = components;
-            return this as TBuilder;
+            if (components == null) throw new ArgumentNullException(nameof(components));
+            _component.SetComponents(components);
+            return GetBuilderToReturn();
+
+        }
+
+        private TBuilder GetBuilderToReturn()
+        {
+            return this as TBuilder ?? throw new InvalidOperationException("Builder instance is not of the expected type");
+
         }
 
         public TBuilder WithTypeDefinition(string typeDefinitionName)
         {
             this.typeDefinitionName = typeDefinitionName;
-            return this as TBuilder;
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithPath(string path)
         {
-            _component.Path = path;
-            return this as TBuilder;
+            _component.SetPath(path);
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithInvalidMessage(string invalidMessage)
         {
-            _component.InvalidMessage = invalidMessage;
-            return this as TBuilder;
+            _component.SetInvalidMessage(invalidMessage);
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithStyle(string style)
         {
-            _component.Style = style;
-            return this as TBuilder;
+            _component.SetStyle(style);
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithClass(string className)
         {
-            _component.Class = className;
-            return this as TBuilder;
+            _component.SetClass(className);
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithParameter(string parameterName, object value)
         {
             _customParameters.Add(parameterName, value);
-            return this as TBuilder;
+            return GetBuilderToReturn();
         }
-
-        //public TBuilder WithFormModel(DynamicFormModel formModel)
-        //{
-        //    _component.FormModel = formModel;
-        //    return this as TBuilder;
-        //}
 
         public TBuilder WithSubElements(params DynamicFormModel[] elements)
         {
             _component.FormModel.SubElements.AddRange(elements);
-            return this as TBuilder;
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithSubElement(DynamicFormModel element)
         {
             _component.FormModel.SubElements.Add(element);
-            return this as TBuilder;
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithString(string label, string path)
         {
             _component.FormModel.SubElements.Add(Factories.FormFactory.StringComponent(label, path));
-            return this as TBuilder;
+            return GetBuilderToReturn();
         }
 
         public TBuilder WithValueChanged(EventCallback<object?> valueChanged)
         {
-            _component.ValueChanged = valueChanged;
-            return this as TBuilder;
+            if (_component?.ValueChanged != null)
+            {
+                _component.SetValueChanged(valueChanged);
+            };
+            return GetBuilderToReturn();
         }
-
-        //public FormComponentBuilder<TComponent, TBuilder> AddElement(Action<TBuilder> configure)
-        //{
-        //    // Create a new builder of the same type
-        //    var childBuilder = new TBuilder();
-
-        //    // Configure the child builder using the provided action
-        //    configure(childBuilder);
-
-        //    WithFormModel(childBuilder.Build());
-
-        //    // Return the original builder
-        //    return this;
-        //}
-
 
         public TBuilder ConfigureFormModel(Action<DynamicFormModel> configure)
         {
-            if (_component.FormModel == null)
+            if (_component == null) { return GetBuilderToReturn(); }
+            
+            if(_component.FormModel == null)
             {
-                _component.FormModel = new DynamicFormModel();
+                var model = new DynamicFormModel(); 
+                _component.SetFormModel(model);
+                configure(model);
             }
-
-            configure(_component.FormModel);
-            return this as TBuilder;
+            return GetBuilderToReturn();
         }
 
 
         public virtual DynamicFormModel Build()
         {
+          
             var model = new DynamicFormModel();
+            if (_component == null) { return model; }
+
             model.Parameters.AddRange(_customParameters);
             model.Parameters.Add(ParameterNames.Path, _component.Path);
-            model.Parameters.Add(ParameterNames.InvalidMesssage, _component.InvalidMessage);
+            model.Parameters.Add(ParameterNames.InvalidMesssage, _component.InvalidMessage ?? string.Empty);
             model.SubElements = _component.FormModel.SubElements;
             return model;
         }
